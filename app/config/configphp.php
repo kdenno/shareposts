16439f700f23967ceb0249f14ed6ced9607522b9
kdenno <kabuyedennis3@gmail.com>
2020-04-13 00:45:16 +0300

    add database file

--- app/config/configphp.php
+++ app/config/configphp.php
@@ -1,14 +1,16 @@
 <?php
 $db_config['db_host']= 'localhost';
-$db_config['db_user']= '_YOUR_USERNAME';
+$db_config['db_user']= 'YOUR_DATABASE_USER';
 $db_config['db_pass'] = 'YOUR_DATABASE_PASSWORD';
-$db_config['db_name'] = 'YOUR_DATABASE_NAME';
+$db_config['db_name'] = 'shareposts';
 foreach($db_config as $key => $value){
   define(strtoupper($key), $value);
 }
 // App Root
 define('APPROOT', dirname(dirname(__FILE__)));
 // URL Root
-define('URLROOT', 'SITE_URL');
+define('URLROOT', 'http://localhost/shareposts');
 // Site Name
-define('SITENAME', 'SITE_NAME');
+define('SITENAME', 'THE_SITE_NAME');
+// version
+define('VERSION', '1.0.0');
